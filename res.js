7cc1506732272629b5bc763ac9bd1992c87fe2a0
Panfilova Daria <93768843+Daria09040@users.noreply.github.com>
2022-05-28 17:48:44 +0300

    Update res.js

--- res.js
+++ res.js
@@ -10,17 +10,19 @@ export default function App() {
       <View style={styles.box}>
         <Text style={{color:'blue', fontSize: 15, top:20, left: 32}}>Новости</Text>
         <Image style={{width: 300, height: 200, top: 50, right: 32}} source={{uri: "https://attuale.ru/wp-content/uploads/2019/01/elitefon.ru_12527.jpg"}} />
-        <Text style={{position:'absolute', top: 270,left:23,fontWeight:"bold", color:"black", fontSize:25}}>Помощь себе при стрессе</Text>
-        <Text style={{position:'absolute' ,top: 320,left:23,fontWeight:"normal", color:"black", fontSize:18}}>
-          1. Возьмите паузу.{'\n'}
-          2. Витаминотерапия.{'\n'}
-          3. Правильное питание. {'\n'}
-          4. Ароматерапия.{'\n'}
-          5. Психологическая помощь. {'\n'}
-          6. Экстрим. {'\n'}
-          7. Займитесь спортом. {'\n'}
-          8. Древнейшая система - йога.{'\n'}
-          9. Чаще улыбайтесь. {'\n'}
+        <Text style={{position:'absolute', top: 250,left:23,fontWeight:"bold", color:"black", fontSize:25}}>Превращаем стресс в {'\n'}своего помощника</Text>
+        <Text style={{position:'absolute' ,top: 330,left:23,fontWeight:"normal", color:"black", fontSize:22,}}>
+        Исследователи Йельского {'\n'}
+        университета заявляют, что {'\n'}
+        люди, которые рассматривают{'\n'}
+        стресс, как возможность {'\n'}
+        личностного роста, отмечают{'\n'}
+        улучшение качества жизни.{'\n'}
+        {'\n'}
+        Сегодня узнаем, как это {'\n'}
+        работает и как увидеть {'\n'}
+        положительные стороны {'\n'}
+        стресса.
         </Text>
       </View>
                     
@@ -29,22 +31,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-    backgroundColor: '#DDABDD',
+    backgroundColor: '#9C9C9C',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'relative'
   },
   box: {
     flex: 0,
-    backgroundColor:'#FFCAC9',
+    backgroundColor:'#E8E8E8',
     width: 350,
     height: 650,
     flexDirection: 'row',
     justifyContent: 'center',
     position: 'relative'
   },
-
 });
